fbf0b391ca3690e40220ced017853e818f9ea192
Jacques de Laval <jacques@de-laval.se>
2018-08-07 00:09:25 +0200

    Remove buffer from StreamList

--- piratepersist.c
+++ piratepersist.c
@@ -10,13 +10,13 @@
 
 struct StreamList {
     unsigned int n_streams;
-    char *_buffer;
     char *streams[1000];  // TODO(Jacques): Make dynamic
 };
 
 struct M3U8ParseContext {
     char *buffer;
-    
+    struct StreamList *streamlst;
+};
 
 void stream_list_print(const struct StreamList *lst)
 {
@@ -30,7 +30,6 @@ void stream_list_free(struct StreamList *lst)
     for (unsigned int i = 0; i < lst->n_streams; i++) {
         free(lst->streams[i]);
     }
-    free(lst->_buffer);
     free(lst);
 }
 
@@ -39,30 +38,31 @@ static size_t stream_list_parse_m3u8(char *buffer, size_t size, size_t nmemb, vo
     size_t i;
 	size_t realsize = size * nmemb;
 	size_t last_line_start = 0;
-	struct StreamList *stream_list = (struct StreamList *)userp;
+	struct M3U8ParseContext *parsectx = (struct M3U8ParseContext *)userp;
+	struct StreamList *stream_list = parsectx->streamlst;
 
 	for (i = 0; i < realsize; i++) {
     	if (buffer[i] == '\n') {
-        	if (stream_list->_buffer != NULL) {
-                buffer[i] = 0;
-				stream_list->streams[stream_list->n_streams] = malloc(strlen(stream_list->_buffer) + strlen(&buffer[last_line_start] + 1));
-				strcpy(stream_list->streams[stream_list->n_streams], stream_list->_buffer);
+        	if (parsectx->buffer != NULL) {
+                buffer[i] = '\0';
+				stream_list->streams[stream_list->n_streams] = malloc(strlen(parsectx->buffer) + strlen(&buffer[last_line_start]) + 1);
+				strcpy(stream_list->streams[stream_list->n_streams], parsectx->buffer);
 				strcat(stream_list->streams[stream_list->n_streams++], &buffer[last_line_start]);
-				free(stream_list->_buffer);
-            	stream_list->_buffer = NULL;
+				free(parsectx->buffer);
+            	parsectx->buffer = NULL;
             } else if (buffer[last_line_start] != '#') {
-                buffer[i] = 0;
-				stream_list->streams[stream_list->n_streams] = malloc(strlen(&buffer[last_line_start])+1);
+                buffer[i] = '\0';
+				stream_list->streams[stream_list->n_streams] = malloc(strlen(&buffer[last_line_start]) + 1);
                 strcpy(stream_list->streams[stream_list->n_streams++], &buffer[last_line_start]);
             }
             last_line_start = i + 1;
     	}
 	}
 	if (last_line_start < realsize && buffer[last_line_start] != '#') {
-    	// TODO(Jacques): Check if _buffer is not NULL
-    	stream_list->_buffer = malloc(realsize - last_line_start + 1);
-    	strncpy(stream_list->_buffer, &buffer[last_line_start], realsize - last_line_start);
-    	stream_list->_buffer[realsize - last_line_start] = 0;
+    	// TODO(Jacques): Check if parsectx->buffer is not NULL
+    	parsectx->buffer = malloc(realsize - last_line_start + 1);
+    	strncpy(parsectx->buffer, &buffer[last_line_start], realsize - last_line_start);
+    	parsectx->buffer[realsize - last_line_start] = '\0';
 	}
 
 	return realsize;
@@ -72,9 +72,11 @@ struct StreamList *get_stream_list(const char *url)
 {
 	CURL *curl_handle = curl_easy_init();
     CURLcode res = CURLE_OK;
-    struct StreamList *stream_list = malloc(sizeof (struct StreamList));
-    stream_list->n_streams = 0;
-    stream_list->_buffer = NULL;
+    struct M3U8ParseContext *parsectx = malloc(sizeof (struct M3U8ParseContext));
+    parsectx->streamlst = malloc(sizeof (struct StreamList));
+    parsectx->streamlst->n_streams = 0;
+    parsectx->buffer = NULL;
+    struct StreamList *lst = parsectx->streamlst;
 
     if (!curl_handle) {
         goto error;
@@ -86,7 +88,7 @@ struct StreamList *get_stream_list(const char *url)
         goto error;
     }
 
-    if (CURLE_OK != (curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)stream_list))) {
+    if (CURLE_OK != (curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)parsectx))) {
         goto error;
     }
 
@@ -95,14 +97,18 @@ struct StreamList *get_stream_list(const char *url)
 		goto error;
     }
     curl_easy_cleanup(curl_handle);
-    return stream_list;
+    free(parsectx->buffer);
+    free(parsectx);
+    return lst;
 
     error:
         if (res != CURLE_OK)
             dbg_print("libcurl", "%s\n", curl_easy_strerror(res));
         if (curl_handle)
             curl_easy_cleanup(curl_handle);
-        stream_list_free(stream_list);
+        free(parsectx->buffer);
+        stream_list_free(parsectx->streamlst);
+        free(parsectx);
         return NULL;
 }
 
