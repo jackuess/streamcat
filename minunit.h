aa944869ad2988c71ba38b0fea4033f08adf8bc7
Jacques de Laval <jacques@de-laval.se>
2018-08-20 23:52:04 +0200

    Simplify and generalize assertion macros

--- minunit.h
+++ minunit.h
@@ -1,35 +1,25 @@
 #ifndef minunit_h_INCLUDED
 #define minunit_h_INCLUDED
 
-#define _GNU_SOURCE
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define ASSERT_TRUE(message, test) do { if (!(test)) return message; } while (0)
-#define ASSERT_EQ(first, second) do { char *message; \
-                                      if (first != second) { \
-                                          asprintf(&message, "%s != %s", #first, #second); \
+#define ASSERT_EQ(first, second) do { if (first != second) { \
+                                          char *message = malloc(1024); \
+                                          const char *template ="%s:%d %s != %s"; \
+                                          int chars_needed = snprintf(message, 1024, template, __FILE__, __LINE__, #first, #second); \
+                                          if (chars_needed >= 1024) { \
+                                              free(message); \
+                                              message = malloc(chars_needed + 1); \
+                                              snprintf(message, chars_needed, template, __FILE__, __LINE__, #first, #second); \
+                                          } \
                                           return message; \
                                       } \
                                     } while (0)
-#define ASSERT_STR_EQ(first, second) do { char *message = assert_str_eq(__FILE__, __LINE__, first, second); \
-                                          ASSERT_TRUE(message, message == NULL); } while (0)
-
-static inline char *assert_str_eq(const char *fn, int ln, const char *first, const char *second)
-{
-    const char* template = "%s:%d \"%s\" != \"%s\"";
-    char *err = malloc(1024);
-    int chars_needed = snprintf(err, 1024, template, fn, ln, first, second);
-    if (chars_needed >= 1024) {
-        free(err);
-        err = malloc(chars_needed + 1);
-        snprintf(err, 1024, template, fn, ln, first, second);
-    }
-    return strcmp(first, second) == 0 ? NULL : err;
-}
+#define ASSERT_STR_EQ(first, second) do { ASSERT_EQ(strcmp(first, second), 0); } while (0)
 
 static inline _Bool run_all_tests(char* (*test_functions[]) ()) {
     char *result;
