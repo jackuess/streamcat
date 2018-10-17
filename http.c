6b8a41165477daed03f0ebe6e1c9ef5a16b9073e
Jacques de Laval <jacques@de-laval.se>
2018-10-17 23:41:11 +0200

    Make HTTP Lib implementation opaque (hide Libcurl)

--- http.c
+++ http.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <curl/curl.h>
+
 #include "http.h"
 #include "output.h"
 
-void response_init_data(struct Response *resp) {
+static void response_init_data(struct SCHTTPResponse *resp) {
     resp->data = malloc(1);
     resp->data[0] = '\0';
     resp->data_size = 0;
@@ -14,7 +16,7 @@ void response_init_data(struct Response *resp) {
 static size_t
 response_append_data(void *data, size_t size, size_t nmemb, void *userp) {
     size_t realsize = size * nmemb;
-    struct Response *resp = (struct Response *)userp;
+    struct SCHTTPResponse *resp = (struct SCHTTPResponse *)userp;
 
     resp->data = realloc(resp->data, resp->data_size + realsize + 1);
     memcpy(&(resp->data[resp->data_size]), data, realsize);
@@ -24,26 +26,26 @@ response_append_data(void *data, size_t size, size_t nmemb, void *userp) {
     return realsize;
 }
 
-struct Response http_get(const char *url) {
+struct SCHTTPResponse http_get(const char *url) {
     CURLcode res = CURLE_OK;
-    struct Response resp = {0};
-    resp._curl = curl_easy_init();
+    struct SCHTTPResponse resp = {0};
+    resp.userp = curl_easy_init();
 
-    if (!resp._curl) {
+    if (!resp.userp) {
         dbg_print("mpdcat", "Unable to init Curl\n");
         goto finally;
     }
 
     if (CURLE_OK !=
-        (res = curl_easy_setopt(resp._curl, CURLOPT_FOLLOWLOCATION, true))) {
+        (res = curl_easy_setopt(resp.userp, CURLOPT_FOLLOWLOCATION, true))) {
         dbg_print("mpdcat", "Unable to set FOLLOWLOCATION\n");
         goto finally;
     }
-    if (CURLE_OK != (res = curl_easy_setopt(resp._curl, CURLOPT_URL, url))) {
+    if (CURLE_OK != (res = curl_easy_setopt(resp.userp, CURLOPT_URL, url))) {
         dbg_print("mpdcat", "Unable to set URL\n");
         goto finally;
     }
-    if (CURLE_OK != (res = curl_easy_setopt(resp._curl,
+    if (CURLE_OK != (res = curl_easy_setopt(resp.userp,
                                             CURLOPT_WRITEFUNCTION,
                                             response_append_data))) {
         dbg_print("mpdcat", "Unable to set WRITEFUNCTION\n");
@@ -51,17 +53,17 @@ struct Response http_get(const char *url) {
     }
     response_init_data(&resp);
     if (CURLE_OK !=
-        (res = curl_easy_setopt(resp._curl, CURLOPT_WRITEDATA, &resp))) {
+        (res = curl_easy_setopt(resp.userp, CURLOPT_WRITEDATA, &resp))) {
         dbg_print("mpdcat", "Unable to set WRITEDATA\n");
         goto finally;
     }
 
-    if (CURLE_OK != (res = curl_easy_perform(resp._curl))) {
+    if (CURLE_OK != (res = curl_easy_perform(resp.userp))) {
         goto finally;
     }
 
-    curl_easy_getinfo(resp._curl, CURLINFO_RESPONSE_CODE, &resp.code);
-    curl_easy_getinfo(resp._curl, CURLINFO_EFFECTIVE_URL, &resp.effective_url);
+    curl_easy_getinfo(resp.userp, CURLINFO_RESPONSE_CODE, &resp.code);
+    curl_easy_getinfo(resp.userp, CURLINFO_EFFECTIVE_URL, &resp.effective_url);
 
 finally:
     if (res != CURLE_OK) {
@@ -72,9 +74,9 @@ finally:
     return resp;
 }
 
-void response_free(struct Response *resp) {
-    if (resp->_curl) {
-        curl_easy_cleanup(resp->_curl);
+void response_free(struct SCHTTPResponse *resp) {
+    if (resp->userp != NULL) {
+        curl_easy_cleanup(resp->userp);
     }
     if (resp->data) {
         free(resp->data);
--- http.h
+++ http.h
@@ -2,20 +2,19 @@
 #define http_h_INCLUDED
 
 #include <stdbool.h>
+#include <stddef.h>
 
-#include <curl/curl.h>
-
-struct Response {
-    CURL *_curl;
+struct SCHTTPResponse {
     bool ok;
     char *effective_url;
     long code;
     char *data;
     size_t data_size;
+    void *userp;
 };
 
-struct Response http_get(const char *url);
-void response_free(struct Response *resp);
+struct SCHTTPResponse http_get(const char *url);
+void response_free(struct SCHTTPResponse *resp);
 
 char *urljoin(const char *base, const char *relative);
 
